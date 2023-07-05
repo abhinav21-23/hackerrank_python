2ba7bb7c3f69026b52fb6ddaaf3c40261468b8a0
Abhinav Gupta <121237303+abhinav21-23@users.noreply.github.com>
2023-07-05 22:38:13 +0530

    Create nested_list.py

---
+++ nested_list.py
@@ -0,0 +1,8 @@
+if __name__ == '__main__':
+    records=[]
+    for i in range(int(input())):
+        name = input()
+        score = float(input())
+        records.append([name,score])
+second_highest = sorted(set([score for name, score in records]))[1]
+print('\n'.join(sorted([name for name, score in records if score == second_highest])))
